8a26cb678a5a70374da491afa40ae9f71788d330
nara-ryoya <nara_ryoya@icloud.com>
2022-07-19 14:25:12 +0900

    refac: precommitを適用

--- ch01/agent.py
+++ ch01/agent.py
@@ -1,15 +1,16 @@
 import numpy as np
 
+
 class Agent:
     def __init__(self, epsilon: float, action_size: int = 10) -> None:
         self.epsilon = epsilon
         self.Qs = np.zeros(action_size)
         self.ns = np.zeros(action_size)
-    
+
     def update(self, action: int, reward: int) -> None:
         self.ns[action] += 1
         self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]
-    
+
     def get_action(self) -> int:
         """epsilon-greedy法に従い、Agentの行動を決定する
 
@@ -18,4 +19,4 @@ class Agent:
         """
         if np.random.rand() < self.epsilon:
             return np.random.randint(0, len(self.Qs))
-        return np.argmax(self.Qs)
+        return int(np.argmax(self.Qs))
--- ch01/bandit.py
+++ ch01/bandit.py
@@ -4,7 +4,7 @@ import numpy as np
 class Bandit:
     def __init__(self, arms: int = 10):
         self.rates = np.random.rand(arms)
-    
+
     def play(self, arm: int) -> int:
         rate = self.rates[arm]
         if rate > np.random.rand():
